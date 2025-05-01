b4f2f911d65064529c9776b058b683214f243227
kvs0102013 <kvs0102013@gmail.com>
2025-05-01 18:41:41 +0500

    task_7.2.3

--- task_7.2.py
+++ task_7.2.py
@@ -1,5 +1,20 @@
-for meters in 100,90,95,87,102:
- if meters % 3 == 0:
-   print(meters, 'Подходит')
- else:
-   print(meters, 'Не подходит')
+# for meters in 100,90,95,87,102:
+#  if meters % 3 == 0:
+#    print(meters, 'Подходит')
+#  else:
+#    print(meters, 'Не подходит')
+
+# Задание 7.2
+
+# for degree in 3, 7, 5, 6, 4:
+#     print(degree ** 2, degree ** 3, degree ** 4)
+
+# Задание 7.2.3
+
+winners = 0
+for ticket in 345, 19, 87, 1020, 421:
+    if ticket % 5 == 0:
+        print(ticket, '- Счастливый билет')
+        winners += 1
+print('Счастливых билетов ', winners)        
+     
