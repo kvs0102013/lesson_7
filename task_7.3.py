de8c686fbe4dd65fb1db1c7be47f5537137a51b5
kvs0102013 <kvs0102013@gmail.com>
2025-05-02 14:16:49 +0500

    lesson_7.3

---
+++ task_7.3.py
@@ -0,0 +1,21 @@
+# Урок 7.3 функция range
+# text = 'Python'  # Цикл while выводит слово 5 раз
+# wordCoid = 0
+# while wordCoid < 5:
+#     print(text)
+#     wordCoid +=1
+    
+# text = 'Python'  # Цикл for с помощью функции range
+# for wordCoud in range(5):
+#     print(text)  
+
+# for number in range(11): # Возведение в квадрат чисел от 1 до 10 с помощью randge диапазона
+#     print(number ** 2)  
+
+totalMonths = int(input ('Сколько денег в месяц будем откладывать?'))
+summa = 0
+for months in range(totalMonths):
+    print('Месяц' , months)
+    money = int(input('Сколько денег откладываем в месяц'  ))
+    summa += money
+print('За', totalMonths , 'накоплено  денег' , summa)    
